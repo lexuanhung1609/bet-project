04f2d18bafaf5ee69f8033423d4b4881ee7cb4de
Hung Le <hung.le@infostatus.com.au>
2021-10-30 20:50:40 +0700

    first init

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import mongoose from 'mongoose';
-import path, { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import authRoutes from './routes/auth.js';
-import { User } from './models/user.js';
+
 import { config } from 'dotenv';
 
 config();
 
-const PORT = process.env.PORT;
-const MONGODB_URL = process.env.MONGODB_URL;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -20,13 +21,12 @@ app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/assets')));
+
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.use(authRoutes);
 
 app.get('/', async (req, res) => {
-  const result = await User.find();
-  console.log(result);
   res.render('index');
 });
 
