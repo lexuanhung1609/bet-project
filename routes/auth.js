c0394c44036cdaf2d872083bda49729f568f5355
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2021-11-05 23:56:40 +0700

    Create login function

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import * as express from 'express';
-import { getSignup, postSignup, getLogin, getForgotPassword, getTempPassword, getResetPassword } from '../controllers/auth.js';
+import { getSignup, postSignup, getLogin, postLogin, getForgotPassword, getTempPassword, getResetPassword } from '../controllers/auth.js';
 const router = express.Router();
 
 router.get('/signup', getSignup);
 router.post('/signup', postSignup);
 router.get('/login', getLogin);
+router.post('/login', postLogin);
 router.get('/forgotpassword', getForgotPassword);
 router.get('/temp-password', getTempPassword);
 router.get('/reset-password', getResetPassword);
