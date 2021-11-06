0dc1b69889058afb214c9ab30d5a1854f8914209
Minh Hiếu Nguyễn <hieu.nguyen200408@vnuk.edu.vn>
2021-11-06 20:55:07 +0700

    Create reset password functions

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
 import * as express from 'express';
-import { getSignup, postSignup, getLogin, postLogin, getForgotPassword, getTempPassword, getResetPassword, getAdminIndex } from '../controllers/auth.js';
+import { getSignup, postSignup, getLogin, getForgotPassword, getTempPassword, getResetPassword, postForgotPassword, postTempPassword, postResetPassword, getAdminIndex, postLogin } from '../controllers/auth.js';
 const router = express.Router();
 
 router.get('/signup', getSignup);
 router.post('/signup', postSignup);
+
 router.get('/login', getLogin);
 router.post('/login', postLogin);
 router.get('/forgotpassword', getForgotPassword);
+router.post('/forgotpassword', postForgotPassword);
+
 router.get('/temp-password', getTempPassword);
+router.post('/temp-password', postTempPassword);
+
 router.get('/reset-password', getResetPassword);
+router.post('/reset-password', postResetPassword);
 
 export default router;
