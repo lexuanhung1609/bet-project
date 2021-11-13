ba1fde18410def10ab0d689728faeb90f9483a4c
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2021-11-13 16:00:39 +0700

    Delete Bet

--- routes/bet.js
+++ routes/bet.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { createBet } from '../controllers/bet.js';
+import { createBet, deleteBet } from '../controllers/bet.js';
 const router = express.Router();
 
 router.post('/create', createBet);
+router.post('/delete', deleteBet);
 
 export default router;
