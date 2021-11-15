47981d707df3593627c2c925cfbb785a28a3adcb
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2021-11-15 10:27:55 +0700

    feature-get bet list (#29)

--- controllers/bet.js
+++ controllers/bet.js
@@ -23,7 +23,7 @@ const deleteBet = async (req, res, next) => {
 
   try {
     const result = await Bet.deleteOne({ _id: id });
-    return res.json(OK([existBet]));
+    return res.json(OK([existedBet]));
   } catch (error) {
     return res.json(FAIL([]));
   }
@@ -35,12 +35,29 @@ const updateBet = async (req, res, next) => {
   const betID = betNew._id;
 
   const findBet = { _id: betID };
-  const newValues = { team1: betNew.team1, team2: betNew.team2, time: betNew.time, status: betNew.status, rate: betNew.rate, winner: betNew.winner };
+  const newValues = {
+    team1: betNew.team1,
+    team2: betNew.team2,
+    time: betNew.time,
+    status: betNew.status,
+    rate: betNew.rate,
+    winner: betNew.winner,
+  };
   const updateBet = await Bet.updateOne(findBet, newValues);
 
   const updatedBet = await Bet.findOne({ _id: betID });
 
   return res.json(OK([updatedBet]));
-}
+};
 
-export { createBet, deleteBet, updateBet };
+const getListBet = async (req, res, next) => {
+  const bodyData = req.body;
+  try {
+    const allBet = await Bet.find();
+    return res.json(OK([allBet]));
+  } catch (error) {
+    return res.json(FAIL());
+  }
+};
+
+export { createBet, deleteBet, updateBet, getListBet };
--- routes/bet.js
+++ routes/bet.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBet, deleteBet, updateBet } from '../controllers/bet.js';
+import { createBet, deleteBet, updateBet, getListBet } from '../controllers/bet.js';
 const router = express.Router();
 
 router.post('/create', createBet);
 router.post('/delete', deleteBet);
 router.post('/update', updateBet);
+router.post('/getlistbet', getListBet);
 
 export default router;
