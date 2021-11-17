d0400139b11fe1b3af8adcadb400c125ae9f4e39
MarcusHoang <71581489+nghiahoang43@users.noreply.github.com>
2021-11-17 21:17:14 +0700

    Create admin index

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 import { render } from 'ejs';
+import { Bet } from '../models/bet.js';
+import { Team } from '../models/team.js';
+import { OK, NOT_FOUND, FAIL } from '../shared/response.js';
 import { User } from '../models/user.js';
 import { randomPassword } from '../utils/password.js';
+// import { forEach } from 'lodash';
 
 const getSignup = (req, res) => {
   res.render('auth/signup', { message: null });
@@ -41,7 +45,6 @@ const postLogin = async (req, res, next) => {
   const password = req.body.password;
 
   if (!email || !password) {
-
     return res.render('auth/login', {
       message: 'Please fill the required field',
     });
@@ -52,8 +55,8 @@ const postLogin = async (req, res, next) => {
 
   if (!emailRegex.test(email)) {
     res.json({
-      message: 'Invalid email'
-    })
+      message: 'Invalid email',
+    });
     return res.render('auth/login', {
       message: 'Invalid email',
     });
@@ -63,8 +66,8 @@ const postLogin = async (req, res, next) => {
 
   if (!result) {
     res.json({
-      message: 'Your email doesn\'t exist!'
-    })
+      message: "Your email doesn't exist!",
+    });
     return res.render('auth/login', {
       message: "Your email doesn't exist!",
     });
@@ -72,26 +75,25 @@ const postLogin = async (req, res, next) => {
 
   if (result.password !== password) {
     res.json({
-      message: 'Incorrect password!'
-    })
+      message: 'Incorrect password!',
+    });
     return res.render('auth/login', {
       message: 'Incorrect password!',
     });
   }
 
   if (result.role === 'ADMIN') {
-    return res.redirect('/admin')
+    return res.redirect('/admin');
   }
-  
-  return res.redirect('/');
 
+  return res.redirect('/');
 };
 
 const getForgotPassword = (req, res) => {
   res.render('auth/forgot-password', {
-    errorMessage: null
-  })
-}
+    errorMessage: null,
+  });
+};
 
 const postForgotPassword = async (req, res) => {
   const reqData = req.body;
@@ -100,19 +102,19 @@ const postForgotPassword = async (req, res) => {
   const profile = await User.findOne({ email: reqEmail });
   if (profile === null) {
     return res.render('auth/forgot-password', {
-      errorMessage: 'Your email does not exist!'
-    })
+      errorMessage: 'Your email does not exist!',
+    });
   } else {
     const passedEmail = encodeURIComponent(reqEmail);
     res.redirect('temp-password?email=' + passedEmail);
   }
-}
+};
 
 const getTempPassword = (req, res, next) => {
   const passedEmail = req.query.email;
   res.render('auth/temp-password', {
     passedEmail: passedEmail.trim(),
-    randomPassword: randomPassword()
+    randomPassword: randomPassword(),
   });
 };
 
@@ -120,9 +122,11 @@ const postTempPassword = async (req, res, next) => {
   const tempPassword = req.body.tempPassword;
   const passedEmail = req.body.email;
 
-
   const profile = await User.findOne({ email: passedEmail.trim() });
-  const updatedPasswordProfile = await User.updateOne({ password: profile.password }, { $set: { password: tempPassword } });
+  const updatedPasswordProfile = await User.updateOne(
+    { password: profile.password },
+    { $set: { password: tempPassword } }
+  );
   return res.redirect('reset-password?email=' + passedEmail);
 };
 
@@ -130,9 +134,9 @@ const getResetPassword = (req, res, next) => {
   const passedEmail = req.query.email;
   res.render('auth/reset-password', {
     passedEmail: passedEmail.trim(),
-    errorMessage: null
+    errorMessage: null,
   });
-}
+};
 
 const postResetPassword = async (req, res, next) => {
   const passedEmail = req.body.email;
@@ -142,7 +146,7 @@ const postResetPassword = async (req, res, next) => {
   if (profile.password != currentPassword) {
     return res.render('auth/reset-password', {
       passedEmail: passedEmail.trim(),
-      errorMessage: 'Your current password is incorrect!'
+      errorMessage: 'Your current password is incorrect!',
     });
   }
 
@@ -152,16 +156,44 @@ const postResetPassword = async (req, res, next) => {
   if (newPassword != confirmNewPassword) {
     return res.render('auth/reset-password', {
       passedEmail: passedEmail.trim(),
-      errorMessage: 'Your password(s) did not match!'
+      errorMessage: 'Your password(s) did not match!',
     });
   }
 
-  const updatedPasswordProfile = await User.updateOne({ password: currentPassword }, { $set: { password: newPassword } });
+  const updatedPasswordProfile = await User.updateOne(
+    { password: currentPassword },
+    { $set: { password: newPassword } }
+  );
   res.redirect('login');
-}
-
-const getAdminIndex = (req, res, next) => {
-  res.render('auth/admin');
 };
 
-export { getSignup, postSignup, postLogin, getLogin, getForgotPassword, getTempPassword, getResetPassword, postForgotPassword, postTempPassword, postResetPassword, getAdminIndex };
+const getAdminIndex = async (req, res, next) => {
+  const bodyData = req.body;
+  const allBet = await Bet.find();
+  const allMatch = {};
+  let match_index = 0;
+
+  const teamInfo = await Promise.all(
+    allBet.map((bet) => {
+        return Team.find({ _id: { "$in": [bet.team1.toString(), bet.team2.toString()] } });
+    })
+  );
+  
+  
+  return res.render('admin/index', { allBet, teamInfo });
+};
+
+
+export {
+  getSignup,
+  postSignup,
+  postLogin,
+  getLogin,
+  getForgotPassword,
+  getTempPassword,
+  getResetPassword,
+  postForgotPassword,
+  postTempPassword,
+  postResetPassword,
+  getAdminIndex,
+};
